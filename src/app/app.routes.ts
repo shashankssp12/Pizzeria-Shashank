14c9c706efe42a8aebc23e1c5b7f0cd8529a2aad
shashankssp12 <shashankspandey12@gmail.com>
2026-08-01 00:33:21 +0530

    developed order pizza page-yet to connect

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,31 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { OrderPizzaPageComponent } from './pages/order-pizza-page/order-pizza-page.component';
+import { BuildPizzaPageComponent } from './pages/build-pizza-page/build-pizza-page.component';
+import { ShoppingCartPageComponent } from './pages/shopping-cart-page/shopping-cart-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"",
+        redirectTo:"home-page",
+        pathMatch:'full'
+    },
+    {
+        path:"home-page",
+        component: HomePageComponent
+    },
+     {
+        path:"order-pizza",
+        component: OrderPizzaPageComponent
+    },
+     {
+        path:"build-pizza",
+        component: BuildPizzaPageComponent
+    },
+    {
+        path:"shopping-cart",
+        component: ShoppingCartPageComponent
+    }
+
+
+];
---
+++ src/app/data/pizza-data.ts
@@ -0,0 +1,115 @@
+// import { Pizza } from '../models/pizza.model';
+
+// In a real app this would come from a backend API (usually via HttpClient).
+// Since we don't have a backend for this project, we keep it as a plain
+// TypeScript array and "fake" the data fetch inside a service.
+export const PIZZA_DATA: Pizza[] = [
+  {
+    id: '0001',
+    type: 'veg',
+    price: 290,
+    name: 'Paneer Tikka',
+    image:
+      'https://thumb9.shutterstock.com/display_pic_with_logo/376831/127528958/stock-photo-delicious-italian-pizza-over-white-127528958.jpg',
+    description:
+      'This is popular italian pizza flavoured with marinated tikka sauce and paneer',
+    ingredients: ['dough/flour', 'pizza sauce', 'pizza sauce seasoning', 'cheese'],
+    topping: ['Paneer', 'Fried Onion', 'Green olive', 'Capsicum', 'Red peprika'],
+  },
+  {
+    id: '0002',
+    type: 'nonveg',
+    price: 350,
+    name: 'Chicken Italiaona',
+    image:
+      'https://thumb7.shutterstock.com/display_pic_with_logo/96886/96886,1274350207,7/stock-photo-pizza-53553874.jpg',
+    description:
+      'This is popular italian pizza flavoured with light sugary taste and creamy touch',
+    ingredients: [
+      'deep dish pizza mix',
+      'pizza sauce',
+      'pizza sauce seasoning',
+      'cheese',
+      'sugar and cinnamon blend',
+      'plain butter',
+    ],
+    topping: ['Pepperoni', 'Chicken Sausage', 'Mushroom', 'Capsicum', 'Black beans'],
+  },
+  {
+    id: '0003',
+    type: 'veg',
+    price: 310,
+    name: 'Veggie Supreme',
+    image:
+      'https://thumb1.shutterstock.com/display_pic_with_logo/1003451/770556520/stock-photo-hot-pizza-with-pepperoni-sausage-on-a-dark-background-with-copy-space-pizza-with-mushrooms-770556520.jpg',
+    description:
+      'This is popular italian pizza flavoured with crushed garlic, with multiple herbs topped up with sweet corn',
+    ingredients: [
+      'deep dish pizza mix',
+      'pizza sauce',
+      'pizza sauce seasoning',
+      'cheese',
+      'garlic herbs',
+      'flavored butter',
+    ],
+    topping: ['Fried Onion', 'Sweet corn', 'Mushroom', 'Capsicum', 'Black olive'],
+  },
+  {
+    id: '0004',
+    type: 'nonveg',
+    price: 400,
+    name: 'Tripple Chicken Feast',
+    image:
+      'https://thumb9.shutterstock.com/display_pic_with_logo/2793292/332497832/stock-photo-mixture-pizza-italian-food-332497832.jpg',
+    description:
+      'This is popular italian pizza flavoured with unique greek dressing topped up with keema and meat ball',
+    ingredients: [
+      'low carb pizza dough',
+      'pizza sauce',
+      'pizza sauce seasoning',
+      'cheese',
+      'greek dressing',
+      'cajun',
+    ],
+    topping: ['Chicken keema', 'Fried Onion', 'Chicken Meat ball', 'Capsicum', 'Sweet corn'],
+  },
+  {
+    id: '0005',
+    type: 'nonveg',
+    price: 625,
+    name: 'Ultimate Chicken',
+    image:
+      'https://thumb7.shutterstock.com/display_pic_with_logo/2793292/246331354/stock-photo-pizza-margherita-italian-246331354.jpg',
+    description:
+      'This is popular italian pizza flavoured with BBQ sauce, flavored butter. It has a spongy base which gives a unique taste with multiple toppings',
+    ingredients: [
+      'deep dish pizza mix',
+      'pizza sauce',
+      'pizza sauce seasoning',
+      'cheese',
+      'BBQ sauce',
+      'cajun',
+      'flavored butter',
+    ],
+    topping: ['Pepperoni', 'Fried Onion', 'Chicken Meat ball', 'Chicken Sausage', 'Chicken keema'],
+  },
+  {
+    id: '0006',
+    type: 'nonveg',
+    price: 400,
+    name: 'Tripple Chicken Feast II',
+    image:
+      'https://thumb9.shutterstock.com/display_pic_with_logo/2793292/332497832/stock-photo-mixture-pizza-italian-food-332497832.jpg',
+    description:
+      'This is popular italian pizza flavoured with unique greek dressing topped up with keema and meat ball',
+    ingredients: [
+      'low carb pizza dough',
+      'pizza sauce',
+      'pizza sauce seasoning',
+      'cheese',
+      'greek dressing',
+      'cajun',
+    ],
+    topping: ['Chicken keema', 'Fried Onion', 'Chicken Meat ball', 'Capsicum', 'Sweet corn'],
+  },
+];
